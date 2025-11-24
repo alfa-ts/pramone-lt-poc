018d223a81374bacf1a1d1b49a05f8e9b16f3a74
Ignas Jasonas - HELMES <ignas.jasonas@helmes.com>
2025-11-24 21:15:27 +0200

    fix: add appId for sanity

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -17,7 +17,7 @@ export default defineCliConfig({
   },
   studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to learn more about using environment variables for local & production.
   deployment: {
-    autoUpdates: true,
-    
+    autoUpdates: false,
+    appId: process.env.SANITY_STUDIO_APP_ID || '',
   },
 })
